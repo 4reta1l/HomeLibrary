b5de36c83717291ecee92906511d129b9acc9a2c
Maksym Pyvovarov <maxpyvovarov@gmail.com>
2025-12-12 19:42:26 +0100

    Small refactor

--- Sources/Screens/MyLibraryView.swift
+++ Sources/Screens/MyLibraryView.swift
@@ -18,40 +18,7 @@ struct MyLibraryView: View {
 
                 List {
                     ForEach(viewModel.books, id: \.id) { book in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(book.title)
-                                    .font(.body)
-                                    .bold()
-                                
-                                HStack {
-                                    if !book.author.isEmpty {
-                                        Text(book.author)
-                                            .font(.caption)
-                                            .foregroundColor(.gray)
-                                    }
-                                    
-                                    Spacer()
-                                    
-                                    Image(systemName: "book.pages")
-                                    Text("\(book.pages)")
-                                        .font(.caption)
-                                        .foregroundColor(.gray)
-                                    
-                                    Image(systemName: "calendar")
-                                    Text("\(book.year)")
-                                        .font(.caption)
-                                        .foregroundColor(.gray)
-                                }
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: book.status.displaySign)
-                                .frame(width: 25, height: 25)
-                                .padding(.trailing, 5)
-                        }
-                        .contentShape(Rectangle())
+                        bookRowView(book)
                     }
                 }
             }
@@ -65,5 +32,42 @@ struct MyLibraryView: View {
         } else {
             Text("\(viewModel.books.count) books")
         }
+    }
+
+    private func bookRowView(_ book: Book) -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(book.title)
+                    .font(.body)
+                    .bold()
+
+                HStack {
+                    if !book.author.isEmpty {
+                        Text(book.author)
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+
+                    Spacer()
+
+                    Image(systemName: "book.pages")
+                    Text("\(book.pages)")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+
+                    Image(systemName: "calendar")
+                    Text("\(book.year)")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+            }
+
+            Spacer()
+
+            Image(systemName: book.status.displaySign)
+                .frame(width: 25, height: 25)
+                .padding(.trailing, 5)
+        }
+        .contentShape(Rectangle())
     }
 }
