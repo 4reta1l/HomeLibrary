fdf387adcda234a71304ba81650b5cf556713c52
Maksym Pyvovarov <maxpyvovarov@gmail.com>
2026-01-13 22:36:02 +0100

    [Core Data model] Create CDPublisher model

---
+++ CoreData/Models/CDPublisher.swift
@@ -0,0 +1,27 @@
+//
+//  CDPublisher.swift
+//  HomeLibrary
+//
+//  Created by Maksym Pyvovarov on 13/01/2026.
+//
+
+import Foundation
+import CoreData
+
+@objc(CDPublisher)
+final class CDPublisher: NSManagedObject {
+
+    static let entityName = "CDPublished"
+
+    @NSManaged var id: UUID
+    @NSManaged var displayName: String
+
+    @NSManaged var books: Set<CDBook>
+}
+
+extension CDPublisher {
+    @nonobjc
+    class func fetchRequest() -> NSFetchRequest<CDPublisher> {
+        NSFetchRequest<CDPublisher>(entityName: entityName)
+    }
+}
