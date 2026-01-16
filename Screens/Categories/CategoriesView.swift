cb77b49a7e003eabca3b8182ea50afadb0e3b79c
Maksym Pyvovarov <maxpyvovarov@gmail.com>
2026-01-16 23:00:39 +0100

    [View] Add add category button

--- Screens/Categories/CategoriesView.swift
+++ Screens/Categories/CategoriesView.swift
@@ -11,6 +11,8 @@ struct CategoriesView: View {
 
     @Environment(LibraryStore.self) private var store
 
+    @State private var showAddCategory: Bool = false
+
     var body: some View {
         NavigationView {
             VStack(spacing: 8) {
@@ -27,8 +29,31 @@ struct CategoriesView: View {
                         }
                     }
                 }
+
+                addCategoryButton
             }
             .navigationTitle("My categories")
         }
+    }
+
+    private var addCategoryButton: some View {
+        Button {
+            showAddCategory = true
+        } label: {
+            Text("Add category")
+                .font(.headline)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.green)
+                .foregroundStyle(.white)
+                .cornerRadius(10)
+                .padding(.horizontal)
+                .padding(.top, -5)
+        }
+       .sheet(isPresented: $showAddCategory) {
+           EditCategoryView { newCategory in
+               store.addCategory(newCategory)
+           }
+       }
     }
 }
